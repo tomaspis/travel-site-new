590679724698ef4987375ca2168109601950e370
Tomáš Píš <tomas.pis@adlerprint.sk>
2023-12-01 15:46:06 +0100

    Updated netlify function - new path

---
+++ netlify/functions/secret-area.js
@@ -0,0 +1,28 @@
+exports.handler = function(event, context, callback) {
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two wquals four.</p>
+    `
+    
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        } )
+    } else {
+
+        callback(null, {
+            statusCode: 401
+        } )
+    }
+
+
+}
